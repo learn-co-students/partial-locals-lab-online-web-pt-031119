af8e23c3eb31172d0bb3d215509306f9d3c64eb7
Kelly Sciandra <kellysciandradev@icloud.com>
2019-08-19 13:18:36 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,7 +7,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 80.times do
-  Student.create(name: Faker::Name.name, hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
+  Student.create(name: Faker::Name.name, hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
 end
 Classroom.create(course_name: 'Math', semester: "Spring #{Time.now.year}")
 Classroom.create(course_name: 'English', semester: "Fall #{Time.now.year}")
